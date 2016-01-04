b3b4b84941a556e0b341873efa656a6e2ccfd6b5
utylee <utylee@gmail.com>
2016-01-04 17:27:02 +0900

    a

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 import sys
+import asyncio
 import win32com
+#from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QAxContainer import *
 from win32com import client 
-#from quamash import QEventLoop
+from quamash import QEventLoop
 
 qt5_url = "C:/cygwin/home/utylee/.virtualenvs/tyTrader-win/Lib/site-packages/PyQt5/plugins" 
 
+# PyQt와 quamash를 이용해서 asyncio 방식을 사용합니다
+# PyQt5를 virtualenv 상에서 사용하기 위해서는 정확하게 Platform 폴더를 지정해줘야 한다고 합니다.
+QCoreApplication.setLibraryPaths([qt5_url])
+app = QApplication(sys.argv)
+loop = QEventLoop(app)
+asyncio.set_event_loop(loop)
+
 class MyWindows(QMainWindow):
-#class MyWindows:
     def __init__(self):
         super().__init__()
 
@@ -18,17 +26,20 @@ class MyWindows(QMainWindow):
 
         #test =win32com.client.Dispatch("KHOPENAPI.KHOpenAPICtrl.1")
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
-        print('테스트 입니당 데헷')
         self.kiwoom.dynamicCall("CommConnect()")
 
-
-if __name__ == "__main__":
-    print('갑니다')
-    #QtCore.QCoreApplication.setLibraryPaths(['C:/cygwin/home/utylee/.virtualenvs/tyTrader-win/Lib/site-packages/PyQt5/plugins'])
-    QCoreApplication.setLibraryPaths([qt5_url])
-
-    app = QApplication(sys.argv)
+with loop:
+    pass
     window = MyWindows()
     window.show()
-    app.exec_()
-#test.CommConnect()
+    loop.run_forever()
+
+'''
+if __name__ == "__main__":
+    QCoreApplication.setLibraryPaths([qt5_url])
+
+    #app = QApplication(sys.argv)
+    window = MyWindows()
+    window.show()
+    #app.exec_()
+'''
