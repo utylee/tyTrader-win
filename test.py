7f0000b7baa8f3b1581961bb7b0c1a633a6499db
utylee <utylee@gmail.com>
2016-01-04 15:34:55 +0900

    a

--- test.py
+++ test.py
@@ -1,6 +1,34 @@
+import sys
 import win32com
-from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QAxContainer import *
 from win32com import client 
-test =win32com.client.Dispatch("KHOPENAPI.KHOpenAPICtrl.1")
-test.CommConnect()
-print('테스트 입니당 데헷')
+#from quamash import QEventLoop
+
+qt5_url = "C:/cygwin/home/utylee/.virtualenvs/tyTrader-win/Lib/site-packages/PyQt5/plugins" 
+
+class MyWindows(QMainWindow):
+#class MyWindows:
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("test")
+
+        #test =win32com.client.Dispatch("KHOPENAPI.KHOpenAPICtrl.1")
+        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        print('테스트 입니당 데헷')
+        self.kiwoom.dynamicCall("CommConnect()")
+
+
+if __name__ == "__main__":
+    print('갑니다')
+    #QtCore.QCoreApplication.setLibraryPaths(['C:/cygwin/home/utylee/.virtualenvs/tyTrader-win/Lib/site-packages/PyQt5/plugins'])
+    QCoreApplication.setLibraryPaths([qt5_url])
+
+    app = QApplication(sys.argv)
+    window = MyWindows()
+    window.show()
+    app.exec_()
+#test.CommConnect()
